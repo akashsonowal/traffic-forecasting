ec8f36e376d17b5d5a89936ee2e57d8f49b521bc
Akash Sonowal <34851159+akashsonowal@users.noreply.github.com>
2023-01-12 08:48:10 +0530

    dataloader commit

--- data_loader/dataloader.py
+++ data_loader/dataloader.py
@@ -31,7 +31,41 @@ class TrafficDataset(InMemoryDataset):
     """
     Dataset for Graph Neural Networks.
     """
-    def 
+    def __init__(self, config, W, root='', transform=None, pre_transform=None):
+        self.config = config
+        self.W = W
+        super().__init__(root, transform, pre_transform)
+        self.data, self.slices, self.n_node, self.mean, self.std_dev = torch.load(self.processed_paths[0])
+
+    @property 
+    def raw_file_names(self):
+        return [os.path.join(self.raw_dir, 'PeMSD7_V_228.csv')]
+
+    @property 
+    def processed_file_names(self):
+        return ['./data.pt']
+    
+    def download(self):
+        copyfile('./dataset/PeMSD7_V_228.csv', os.path.join(self.raw_dir, 'PeMSD7_V_228.csv'))
+    
+    def process(self):
+        """
+        Process the raw datasets into saved .pt dataset for later use.
+        Note that any self.fiels won't exist if loading straight from .pt file
+        """
+        #Data preprocessing and loading
+        data = pd.read_csv(self.raw_file_names[0], header=None).values
+        #Technically using the validation and test datasets here, but it is fine, would normally
+        #get the mean and std_dev from a large dataset
+        mean = np.mean(data)
+        std_dev = np.std(data)
+        data = z_score(data, np.mean(data), np.std(data))
+
+        _, n_node = data.shape
+        n_window = self.config['N_PRED'] + self.config['N_HIST']
+
+        #manipulate n x n matrix into 2 x num_edges
+
 
 
 
