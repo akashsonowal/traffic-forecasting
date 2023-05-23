01e22af4334f8a282317a6ad29c2dd97fc853801
Akash Sonowal <work.akashsonowal@gmail.com>
2023-05-23 06:43:44 +0530

    update

--- traffic_forecasting/utils.py
+++ traffic_forecasting/utils.py
@@ -1,5 +1,42 @@
 import torch
 
+def distance_to_weight(W, sigma2=0.1, epsilon=0.5, gat_version=False):
+    """
+    Given distances between all nodes, convert into a weight matrix
+    :param  W distances
+    :param sigma2 user configurable parameter to adjust sparsity of matrix
+    :param epsilon user configurable parameter to adjust sparsity of matrix
+    :param gat_version If true, use 0/1 weights with self loops. Otherwise use float
+    """
+    n = W.shape[0]
+    W = W / 1000.0
+    W2, W_mask = W * W, np.ones([n, n]) - np.identity(n)
+    W = (
+        np.exp(-W2 / sigma2) * (np.exp(-W2 / sigma2) >= epsilon) * W_mask
+    )  # thresholded gaussian kernel method
+
+    # If using the gat version of this, round to 0/1 and include self loops
+    if gat_version:
+        W[W > 0] = 1
+        W += np.identity(n)
+
+    return W
+
+def get_splits(dataset: TrafficDataset, n_slot, splits):
+    """
+    Given the data, split it into random subsets of train, val and test as given by splits
+    :param dataset: TrafficDataset object to split
+    :param n_slot: Number of possible sliding windows in a day
+    :param splits: (train, val, test) ratios
+    """
+    split_train, split_val, _ = splits
+    i = n_slot * split_train
+    j = n_slot * split_val
+    train = dataset[:i]
+    val = dataset[i : i + j]
+    test = dataset[i + j :]
+
+    return train, val, test
 
 def z_score(x, mean, std):
     """
