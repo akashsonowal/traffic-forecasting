c24d93e78ab14d13bab11ec23794d95a8a6ccf60
Akash Sonowal <work.akashsonowal@gmail.com>
2023-05-23 23:53:47 +0530

    Update utils.py

--- traffic_forecasting/utils.py
+++ traffic_forecasting/utils.py
@@ -46,19 +46,6 @@ def get_splits(dataset: TrafficDataset, n_slot, splits):
     return train, val, test
 
 
-def z_score(x, mean, std):
-    """
-    Z-score normalization function: $Z = (X - \mu) / \sigma $
-    where z is the z-score, X is the value of the element,
-    $\mu$ is the population mean, and $\sigma$ is the standard deviation.
-    :param x: torch array, input array to be normalized.
-    :param mean: float, the value of mean.
-    :param std: float, the value of standard deviation.
-    :return: torch array, the z-score normalized array.
-    """
-    return (x - mean) / std
-
-
 def un_z_score(x_normed, mean, std):
     """
     Undo the z-score calculation
